7490fb72844fd2a380b8fa6a9753830767f51caf
Feather Knee <fknee@nvidia.com>
2019-09-09 22:13:46 -0700

    add inner values to tag

--- src/Tag/Tag.js
+++ src/Tag/Tag.js
@@ -2,9 +2,14 @@ import React from 'react'
 import { FiX } from 'react-icons/fi'
 import './tag.css'
 
-export const Tag = ({ children, onClick, onMouseEnter }) => (
-	<span className='ui tag' onClick={onClick} onMouseEnter={onMouseEnter}>
+export const Tag = ({ children, close, onClick, onMouseEnter, onMouseLeave }) => (
+  <span
+    className='ui tag'
+    onClick={onClick}
+    onMouseEnter={onMouseEnter}
+    onMouseLeave={onMouseLeave}
+  >
 		{children}
-		<FiX />
+		<FiX onClick={close} />
 	</span>
 )
